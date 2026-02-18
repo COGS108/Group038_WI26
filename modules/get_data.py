d7d1535530c1b5bbb3aabfddf1a8d4c01ab0c43d
Andy <kgh3122@gmail.com>
2026-02-17 20:34:09 -0800

    added scrap code for steam250 see /modules/get_data.py

--- modules/get_data.py
+++ modules/get_data.py
@@ -1,6 +1,8 @@
 import os
 import requests
 from tqdm import tqdm
+from bs4 import BeautifulSoup
+import pandas as pd
 
 def get_raw(file_list, destination_directory):
     """
@@ -41,3 +43,70 @@ def get_raw(file_list, destination_directory):
             print(f"Error downloading {filename}: {e}")
         except Exception as e:
             print(f"An unexpected error occurred for {filename}: {e}")
+
+def get_steam250(years, destination_directory):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    for year in tqdm(years):
+        url = f'https://steam250.com/{year}'
+
+        try:
+            html = requests.get(url, headers=headers).text
+            soup = BeautifulSoup(html, 'html.parser')
+
+            section = soup.select_one("section.applist.compact")
+            rows = section.find_all("div", id=True)
+
+            records = []
+
+            for row in rows:
+                try:
+                    rank_div = row.find_all('div', recursive=False)[0]
+                    texts = [t.strip() for t in rank_div.contents if isinstance(t, str) and t.strip()]
+                    if texts:
+                        rank = int(texts[0])
+                    else:
+                        continue
+                    
+                    name_tag = row.select_one("a[title]")
+                    game = name_tag.text.strip()
+
+                    company_tag = row.select_one("span.company")
+                    company = company_tag.text.strip() if company_tag else ""
+
+                    score_tag = row.select_one("span.score")
+                    score = score_tag.text.strip() if score_tag else ""
+
+                    rating_tag = row.select_one("span.rating")
+                    rating = rating_tag.text.strip() if rating_tag else ""
+
+                    vote_tag = row.select_one("span.votes")
+
+                    rating = ""
+
+                    if rating_tag:
+                        rating = rating_tag.text.strip()
+                        if vote_tag:
+                            rating = f"{rating} {vote_tag.text.strip()}"
+                    records.append({'Rank': rank, 'Game': game, 'Company': company, 'Score': score, 'Rating': rating})
+                except:
+                    continue
+            
+            df = pd.DataFrame(records)
+            output = os.path.join(destination_directory, f'{year}_top250.csv')
+            df.to_csv(output, index=False)
+            print(f'saved file {year}_top250.csv')
+        
+        except Exception as e:
+            print(f"error find on {year}: {e}")
+
+if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    rawdata_dir = os.path.join(script_dir, "..", "data", "00-raw")
+
+    years = [2018, 2019, 2020, 2021, 2022, 2023]
+    get_steam250(years, rawdata_dir)
+
+
