02fb730ddc4001855dac383a2cc3ed44593baa80
TeaFive <vo.ashley.t@gmail.com>
2026-02-18 14:24:56 -0800

    Update Steam250 fetching and change Kaggle dataset to one with IDs

--- modules/get_data.py
+++ modules/get_data.py
@@ -3,6 +3,8 @@ import requests
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 import pandas as pd
+import kagglehub
+import shutil
 
 def get_raw(file_list, destination_directory):
     """
@@ -45,64 +47,82 @@ def get_raw(file_list, destination_directory):
             print(f"An unexpected error occurred for {filename}: {e}")
 
 def get_steam250(years, destination_directory):
-    for year in tqdm(years):
+    for year in years:
         url = f'https://steam250.com/{year}'
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
 
-        try:
-            html = requests.get(url).text
-            soup = BeautifulSoup(html, 'html.parser')
+        section = soup.select_one("section.applist.compact")
+        rows = section.find_all("div", id=True)
 
-            section = soup.select_one("section.applist.compact")
-            rows = section.find_all("div", id=True) 
+        records = []
 
-            records = []
+        for row in rows:
+            try:
+                rank_div = row.find_all("div", recursive=False)[0]
 
-            for row in rows:
-                try:
-                    rank_div = row.find_all('div', recursive=False)[0]
-                    texts = [t.strip() for t in rank_div.contents if isinstance(t, str) and t.strip()]
-                    if texts:
-                        rank = int(texts[0])
-                    else:
-                        continue
-                    
-                    name_tag = row.select_one("a[title]")
-                    game = name_tag.text.strip()
-
-                    company_tag = row.select_one("span.company")
-                    if company_tag:
-                        company = company_tag.text.strip()
-                    else:
-                        company = ""
-
-                    score_tag = row.select_one("span.score")
-                    if score_tag:
-                        score = score_tag.text.strip()
-                    else:
-                        score = ""
-
-                    rating_tag = row.select_one("span.rating")
-                    vote_tag = row.select_one("span.votes")
-
-                    if rating_tag:
-                        rating = rating_tag.text.strip()
-                        if vote_tag:
-                            rating = f"{rating} {vote_tag.text.strip()}"
-                    records.append({'Rank': rank, 'Game': game, 'Company': company, 'Score': score, 'Rating': rating})
-                except:
+                # Ignore rank change stats e.g. "+2" or "-1"
+                texts = [t.strip() for t in rank_div.contents if isinstance(t, str) and t.strip()]
+                if texts:
+                    rank = int(texts[0])
+                else:
                     continue
-            
-            df = pd.DataFrame(records)
-            output = os.path.join(destination_directory, f'{year}_top250.csv')
-            df.to_csv(output, index=False)
-            print(f" saved file {year}_top250.csv")
-        
-        except Exception as e:
-            print(f" error find on {year}: {e}")
 
-if __name__ == "__main__":
+                name_tag = row.select_one("a[title]")
+                name = name_tag.text.strip()
+
+                app_link = name_tag["href"]
+                appid = int(app_link.split("/")[-1])
+
+                rating = row.select_one("span.rating").text.strip().replace("%","")
+                rating = float(rating)
+
+                votes_raw = row.select_one("span.votes")["title"]
+                votes = votes_raw.replace(",", "")
+                votes = votes.replace("reviews", "")
+                votes = votes.replace("\"", "")
+                votes = int(votes)
+
+                records.append({
+                    "rank": rank,
+                    "appid": appid,
+                    "name": name,
+                    "rating": rating,
+                    "num_votes": votes
+                })
+
+            except Exception as e:
+                print(e)
+                continue
+
+        curr_df = pd.DataFrame(records)
+        curr_file_name = os.path.join(destination_directory, f'{year}_top250.csv') 
+
+        curr_df.to_csv(curr_file_name, index=False)
+
+    # Done message
+    print("Top 250 Steam games for 2018-2023 installed!")
+
+def get_kaggle(): 
+    target_dir = "./data/00-raw"
+    path = kagglehub.dataset_download("srgiomanhes/steam-games-dataset-2025")
+    
+    os.makedirs(target_dir, exist_ok= True)
+
+    # Ignore kagglehub download directory structure
+    for file in os.listdir(path):
+        shutil.move(os.path.join(path, file), target_dir)
+
+    print("Steam 2025 dataset installed!")
+
+
+def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     rawdata_dir = os.path.join(script_dir, "..", "data", "00-raw")
 
     years = [2018, 2019, 2020, 2021, 2022, 2023]
     get_steam250(years, rawdata_dir)
+    get_kaggle()
+
+if __name__ == "__main__":
+    main()
